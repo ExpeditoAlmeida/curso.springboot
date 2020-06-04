c5bc8b5b9d22f4ad54fdce2164de3d28e874eb68
exped <exped@DESKTOP-UK83BO2>
2020-06-04 16:33:17 -0300

    Ajuste da interfacee  e controller da classe pessoa. Impl do crud.

--- curso.springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ curso.springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package curso.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
@@ -16,11 +22,13 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 
@@ -30,7 +38,8 @@ public class PessoaController {
 		andView.addObject("pessoas", pessoasIt);
 
 		////////////////////////////////////
-
+		// objeto vazio para o form trabalhar
+		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
 
@@ -39,6 +48,44 @@ public class PessoaController {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoasIt);
+		// objeto vazio para o form trabalhar
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+
+	// "/editarpessoa/{idpessoa} url é paramentro da tela" //anotação @GetMapping
+	// substitui @RequestMapping(method = RequestMethod.GET
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+
+		// carrega o objeto do banco
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		// prepara o retorno para a tela designada
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+
+		andView.addObject("pessoaobj", pessoa.get());
+
+		return andView;
+	}
+
+	@GetMapping("/removerpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+
+		pessoaRepository.deleteById(idpessoa);
+		// prepara o retorno para a tela designada
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+
+		andView.addObject("pessoas", pessoaRepository.findAll());
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas", pessoaRepository.findPessoaBynome(nomepesquisa));
+		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
 }
