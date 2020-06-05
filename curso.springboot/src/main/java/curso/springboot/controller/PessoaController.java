fbbcf91ad032339bbb9e4ca7fa7608b95ae7ff39
exped <exped@DESKTOP-UK83BO2>
2020-06-05 18:47:03 -0300

    Implementação e mapeamento na classe Telefone; Implementação do crud na classe Telefone; Ajuste na interface;

--- curso.springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ curso.springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
+import curso.springboot.model.Telefone;
 import curso.springboot.repository.PessoaRepository;
+import curso.springboot.repository.TelefoneRepository;
 
 @Controller
 public class PessoaController {
@@ -21,9 +23,14 @@ public class PessoaController {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
+	@Autowired
+	private TelefoneRepository telefoneRepository;
+
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public ModelAndView inicio() {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
 		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
@@ -86,6 +93,49 @@ public class PessoaController {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		andView.addObject("pessoas", pessoaRepository.findPessoaBynome(nomepesquisa));
 		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+
+	@GetMapping("/telefones/{idpessoa}")
+	public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa) {
+
+		// carrega o objeto do banco
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		// prepara o retorno para a tela designada
+		ModelAndView andView = new ModelAndView("cadastro/telefones");
+
+		andView.addObject("pessoaobj", pessoa.get());
+		// ação para carregar a lista de telefone da respectiva pessoa
+		andView.addObject("telefones", telefoneRepository.getTelefones(idpessoa));
+		return andView;
+	}
+
+	@PostMapping("**/addfonePessoa/{pessoaid}")
+	public ModelAndView addFonePessoa(Telefone telefone, @PathVariable("pessoaid") Long pessoaid) {
+		Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
+		telefone.setPessoa(pessoa);
+		telefoneRepository.save(telefone);
+
+		ModelAndView andView = new ModelAndView("cadastro/telefones");
+		andView.addObject("pessoaobj", pessoa);
+		andView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+		return andView;
+
+	}
+
+	@GetMapping("/removertelefone/{idtelefone}")
+	public ModelAndView removertelefone(@PathVariable("idtelefone") Long idtelefone) {
+
+		Pessoa pessoa = telefoneRepository.findById(idtelefone).get().getPessoa();
+
+		telefoneRepository.deleteById(idtelefone);
+
+		ModelAndView andView = new ModelAndView("cadastro/telefones");
+
+		andView.addObject("pessoas", pessoaRepository.findAll());
+		andView.addObject("pessoaobj", pessoa);
+		andView.addObject("telefones", telefoneRepository.getTelefones(pessoa.getId()));
 		return andView;
 	}
 }
--- curso.springboot/src/main/java/curso/springboot/model/Pessoa.java
+++ curso.springboot/src/main/java/curso/springboot/model/Pessoa.java
@@ -1,11 +1,14 @@
 package curso.springboot.model;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -21,6 +24,9 @@ public class Pessoa implements Serializable {
 	private String sobrenome;
 
 	private int idade;
+
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)
+	private List<Telefone> telefones;
 
 	public Long getId() {
 		return id;
@@ -52,6 +58,14 @@ public class Pessoa implements Serializable {
 
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
 	}
 
 }
